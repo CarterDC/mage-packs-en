060b9e6b2a44155530d83d88df5bf289e7fdecfa
CarterDC <carter_dc99@hotmail.com>
2021-10-21 22:40:42 +0200

    moar images

--- scripts/mage-packs.js
+++ scripts/mage-packs.js
@@ -1,6 +1,6 @@
 
 Hooks.once('init', () => {
 
-  console.log('%cMage-Packs-Fr %c| Initialisé.', 'color: #22eeee', 'color: #ccc');
+  console.log('%cMage-Packs-En %c| Initialisé.', 'color: #22eeee', 'color: #ccc');
 
 });
